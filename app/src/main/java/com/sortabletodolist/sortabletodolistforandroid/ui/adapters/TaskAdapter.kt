ec88c34baf3544266b44547bc11bf5e796f9b6b1
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-04-23 20:52:16 +0300

    fix: when you add too many tasks and they may overlap with the text

--- app/src/main/java/com/sortabletodolist/sortabletodolistforandroid/ui/adapters/TaskAdapter.kt
+++ app/src/main/java/com/sortabletodolist/sortabletodolistforandroid/ui/adapters/TaskAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.CheckBox
 import android.widget.TextView
 import com.sortabletodolist.domain.models.Task
 import com.sortabletodolist.sortabletodolistforandroid.R
@@ -41,9 +42,13 @@ class TaskAdapter(private val tasks: List<Task>) : BaseAdapter()
         val task = getItem(position) as Task
         val taskNameTextView = view.findViewById<TextView>(R.id.taskNameTextView)
         val taskTextTextView = view.findViewById<TextView>(R.id.taskTextTextView)
+        val taskCompleateStatusView = view.findViewById<CheckBox>(R.id.taskStatusCheckBox)
 
         taskNameTextView.text = task.name
         taskTextTextView.text = task.text
+        taskCompleateStatusView.isChecked = task.isCompleted
+        taskCompleateStatusView.isClickable = false
+        taskCompleateStatusView.isFocusable = false
 
         return view
     }
